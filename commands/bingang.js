32d8e2d41752fedeb17fceaad0148fb2502b79db
godjaku <0101rgb@gmail.com>
2016-10-06 12:28:47 +0900

    change type

--- commands/bingang.js
+++ commands/bingang.js
@@ -8,11 +8,11 @@ var findClass= function(day, building){
 
   var Schema= mongoose.Schema;
   var emptySchema= new Schema({ "_id" : mongoose.ObjectId, "building" : String, "room" : String,
-    "mon1" : Integer, "mon2" : Integer, "mon3" : Integer, "mon4" : Integer, "mon5" : Integer, "mon6" : Integer, "mon7" : Integer,
-    "tue1" : Integer, "tue2" : Integer, "tue3" : Integer, "tue4" : Integer, "tue5" : Integer, "tue6" : Integer, "tue7" : Integer,
-    "wed1" : Integer, "wed2" : Integer, "wed3" : Integer, "wed4" : Integer, "wed5" : Integer, "wed6" : Integer, "wed7" : Integer,
-    "thu1" : Integer, "thu2" : Integer, "thu3" : Integer, "thu4" : Integer, "thu5" : Integer, "thu6" : Integer, "thu7" : Integer,
-    "fri1" : Integer, "fri2" : Integer, "fri3" : Integer, "fri4" : Integer, "fri5" : Integer, "fri6" : Integer, "fri7" : Integer }, {collection: 'empty'})
+    "mon1" : int, "mon2" : int, "mon3" : int, "mon4" : int, "mon5" : int, "mon6" : int, "mon7" : int,
+    "tue1" : int, "tue2" : int, "tue3" : int, "tue4" : int, "tue5" : int, "tue6" : int, "tue7" : int,
+    "wed1" : int, "wed2" : int, "wed3" : int, "wed4" : int, "wed5" : int, "wed6" : int, "wed7" : int,
+    "thu1" : int, "thu2" : int, "thu3" : int, "thu4" : int, "thu5" : int, "thu6" : int, "thu7" : int,
+    "fri1" : int, "fri2" : int, "fri3" : int, "fri4" : int, "fri5" : int, "fri6" : int, "fri7" : int }, {collection: 'empty'})
   var db= mongoose.connection;
   var empty= mongoose.model('empty', emptySchema);
   empty.find({"building":building, day:0}, function(err, docs){
